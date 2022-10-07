b1bcc40137b0e4325bed2c94f325c3095e9c8450
gagdiez <gagdiez.c@gmail.com>
2022-10-07 16:29:01 +0200

    more rules

---
+++ integration-tests/src/main.ava.ts
@@ -0,0 +1,71 @@
+import { Worker, NEAR, NearAccount } from 'near-workspaces';
+import anyTest, { TestFn } from 'ava';
+
+const test = anyTest as TestFn<{
+  worker: Worker;
+  accounts: Record<string, NearAccount>;
+}>;
+
+test.beforeEach(async (t) => {
+  // Init the worker and start a Sandbox server
+  const worker = await Worker.init();
+
+  // deploy contract
+  const root = worker.rootAccount;
+
+  // some test accounts
+  const alice = await root.createSubAccount("alice");
+  const guestBook = await root.createSubAccount("guesbook");
+
+  // Get wasm file path from package.json test script in folder above
+  await guestBook.deploy("./base/target/wasm32-unknown-unknown/release/base.wasm");
+
+  // add messages
+  await guestBook.call(guestBook, "add_message", { text: "hola" });
+  await alice.call(guestBook, "add_message", { text: "adios" }, { attachedDeposit: NEAR.parse('1') });
+
+
+  // Save state for test runs, it is unique for each test
+  t.context.worker = worker;
+  t.context.accounts = { root, guestBook, alice };
+});
+
+test.afterEach(async (t) => {
+  // Stop Sandbox server
+  await t.context.worker.tearDown().catch((error) => {
+    console.log("Failed to stop the Sandbox:", error);
+  });
+});
+
+test("by default it gives the messages two total", async (t) => {
+  const { guestBook, alice } = t.context.accounts;
+
+  const msgs = await guestBook.view("get_messages");
+
+  const expected = [
+    { premium: false, sender: guestBook.accountId, text: "hola" },
+    { premium: true, sender: alice.accountId, text: "adios" },
+  ];
+
+  t.deepEqual(msgs, expected);
+});
+
+test("after the migration there is payment information", async (t) => {
+  const { guestBook, alice } = t.context.accounts;
+
+  await guestBook.deploy("./first-update/target/wasm32-unknown-unknown/release/first_update.wasm");
+  await guestBook.call(guestBook, "migrate", {});
+
+  const msgs = await guestBook.view("get_messages");
+  const payments = await guestBook.view("get_payments");
+
+  const expected_msgs = [
+    { premium: false, sender: guestBook.accountId, text: "hola" },
+    { premium: true, sender: alice.accountId, text: "adios" },
+  ];
+
+  const expected_payments = ["0", NEAR.parse("0.1").toString()];
+
+  t.deepEqual(msgs, expected_msgs);
+  t.deepEqual(payments, expected_payments);
+});
